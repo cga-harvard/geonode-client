8535bd00d11885ffde6b73142332e724cc202783
Matt Bertrand <mbertrand@cga.harvard.edu>
2011-03-23 12:12:13 -0400

    Make feature query panel wider

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -1142,6 +1142,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
         });
 
 		this.gxSearchBar = new GeoExplorer.SearchBar(this);
+
 		var searchPanel = new Ext.Panel({
 			anchor: "100% 5%",
 			items: [this.gxSearchBar]
@@ -1226,7 +1227,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             items: [gridWinPanel, gridResultsPanel],
             collapsed: true,
             region: "east",
-            width: 250
+            width: 350
         });
 
 
